c86dfef777e11a38382825dc0a90cb4daf6f3c5e
Priscila Provetti <priscilaprovetti@gmail.com>
2023-05-05 12:04:58 -0300

    Service layer, component registration

--- src/main/java/com/cursospring/CursoSpringNelio/repositories/UserRepository.java
+++ src/main/java/com/cursospring/CursoSpringNelio/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.cursospring.CursoSpringNelio.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cursospring.CursoSpringNelio.entities.User;
 
+@Repository // opcional anotation
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
