0b7c58267266cbd538e9027c348b7caf60390900
Priscila Provetti <priscilaprovetti@gmail.com>
2023-05-05 10:12:32 -0300

    first commit

---
+++ src/main/java/com/cursospring/CursoSpringNelio/CursoSpringNelioApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.CursoSpringNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursospring/CursoSpringNelio/CursoSpringNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.CursoSpringNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
