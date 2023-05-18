49fe35c0c9067c1895b06163732f4d2832cb73e4
Priscila Provetti <priscilaprovetti@gmail.com>
2023-05-18 15:34:37 -0300

    Payment, one-to-one association

--- src/main/java/com/cursospring/CursoSpringNelio/config/TestConfig.java
+++ src/main/java/com/cursospring/CursoSpringNelio/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursospring.CursoSpringNelio.entities.Category;
 import com.cursospring.CursoSpringNelio.entities.Order;
 import com.cursospring.CursoSpringNelio.entities.OrderItem;
+import com.cursospring.CursoSpringNelio.entities.Payment;
 import com.cursospring.CursoSpringNelio.entities.Product;
 import com.cursospring.CursoSpringNelio.entities.User;
 import com.cursospring.CursoSpringNelio.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1= new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1); //associando o pedido 1 com pagto 1
+		
+		orderRepository.save(o1);
 		
 		
 		
